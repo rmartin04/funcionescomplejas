8bf73a4c46ab6ff40e76838475b44a1129c41056
rmartin04 <ricarditomartin1979+1@gmail.com>
2025-01-14 19:57:02 +0100

    aaa

--- src/HerramientasNumeros.java
+++ src/HerramientasNumeros.java
@@ -1,16 +1,44 @@
 public class HerramientasNumeros {
 
-    public static void mostrarCollatz(int numero){
+    public static void mostrarCollatz(int numero) {
 
-        while(numero != 1){
-            if(numero % 2 == 0){
-                numero = numero/2;
-            }else{
-                numero = numero * 3 + 1;
+        while (numero != 1) {
 
-            }
-            System.out.println(numero);
+           // String mensaje = getMensajeCollatz(numero);
+
+           // System.out.println(mensaje);
+            int num = getNumeroCollatz(numero);
+            numero =num;
+            System.out.println(num);
         }
     }
 
+    private static String getMensajeCollatz(int numero) {
+        String mensaje = "";
+
+        if (numero % 2 == 0) {
+            numero = numero / 2;
+            mensaje = "Par : " + numero;
+        } else {
+            numero = numero * 3 + 1;
+            mensaje = "Impar : " + numero;
+            
+        }
+        return mensaje;
+    }
+
+    
+    private static int getNumeroCollatz(int numero) {
+        String mensaje = "";
+
+        if (numero % 2 == 0) {
+            numero = numero / 2;
+           
+        } else {
+            numero = numero * 3 + 1;
+           
+            
+        }
+        return numero;
+    }
 }
