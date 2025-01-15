8d971bc6f3c0d6c28acbdbf9d927f150fb21cfa6
rmartin04 <ricarditomartin1979+1@gmail.com>
2025-01-15 17:14:12 +0100

    revision ejercicio juego arrays

--- src/EjecucionArrays.java
+++ src/EjecucionArrays.java
@@ -1,16 +1,25 @@
 import java.util.Scanner;
 
-
 public class EjecucionArrays {
     public static void main(String[] args) {
-        
-       // Scanner teclado = new Scanner(System.in);
 
-        int [] numerosArray ={100,500,399};
+        Scanner teclado = new Scanner(System.in);
 
-      JuegoArrays.sumarNumerosArray(numerosArray);
-      
+        int[] numerosArray = new int[3];
+        for (int i = 0; i < numerosArray.length; i++) {
+
+            System.out.println("Introduzca el valor para la posicion :" + i);
+
+            numerosArray[i] = teclado.nextInt();
+
+        }
+        // for (int valores : numerosArray) {
+        //     System.out.println(valores);
+        // }
+
+        int suma = JuegoArrays.sumarNumerosArray(numerosArray);
+        System.out.println("La suma de los valores del array es : " + suma);
+
     }
-
 
 }
--- src/JuegoArrays.java
+++ src/JuegoArrays.java
@@ -1,18 +1,15 @@
 public class JuegoArrays {
 
-    public static void sumarNumerosArray(int[]numeros){
+    public static int sumarNumerosArray(int[] numeros) {
 
-       // int [] numerosArray ={2,4,6,8,10};
         int suma = 0;
-        for(int i = 0; i < numeros.length;i++){
+        for (int i = 0; i < numeros.length; i++) {
 
             suma += numeros[i];
 
         }
-         
-        System.out.println("La suma de los valores del array es: "+suma);
-        
 
+        return suma;
 
     }
 
