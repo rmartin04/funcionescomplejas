dc5e188da6f8535225f34d2a76754b0df09a93e6
rmartin04 <ricarditomartin1979+1@gmail.com>
2025-01-15 16:20:55 +0100

    a

--- src/EjecucionArrays.java
+++ src/EjecucionArrays.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class EjecucionArrays {
     public static void main(String[] args) {
         
-        Scanner teclado = new Scanner(System.in);
+       // Scanner teclado = new Scanner(System.in);
 
         int [] numerosArray ={5,10,15};
         int suma = 0;
